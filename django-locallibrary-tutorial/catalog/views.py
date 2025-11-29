dae54b8d5230ebf4fbab41b872ce3e9f171d1964
kliu67 <47793951+kliu67@users.noreply.github.com>
2025-11-28 23:03:59 -0500

    no message

--- django-locallibrary-tutorial/catalog/views.py
+++ django-locallibrary-tutorial/catalog/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
+def index(request):
+    """View function for home page of site."""
+
+    #Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    #Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    #The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instacnes_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    #Render the HTML template index.html with the data in the context vairable
+
+    return render(request, 'index.html', context=context)
 # Create your views here.
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+    context_object_name = 'book_list'   # your own name for the list as a template variable
+    queryset = Book.objects.filter(title__icontains='') # Get 5 books containing the title war
+    template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+    context_object_name = 'author_list'
+    queryset = Author.objects.all()
+    template_name = 'authors/my_arbitrary_template_author_list.html'
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
